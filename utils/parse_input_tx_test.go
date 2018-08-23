d05cee0f1e560cf64aa29584a1a9fa8436440bed
Ran <skylab.ran@gmail.com>
2018-08-23 16:17:35 +0700

    utils: Added file unit test for function ConvertInputValueWithDecimal (2 test cases)

--- utils/parse_input_tx_test.go
+++ utils/parse_input_tx_test.go
@@ -8,36 +8,74 @@ import (
 )
 
 func TestParseInputTx(t *testing.T) {
-
+	type args struct {
+		input    string
+		decimals int8
+	}
 	tests := []struct {
 		name string
-		args string
+		args args
 		want *types.InputData
 	}{
-		{"Test case 1",
-			"0xa9059cbb0000000000000000000000005f099b04f1fcea17718e6b91e90990c6849055ee0000000000000000000000000000000000000000000000015a3d9f556fd9d800",
+		{
+			"Test case 1: Transaction VECHAIN",
+			args{
+				input:    "0xa9059cbb00000000000000000000000019bbb1c4407dcee04059682949f32cb3b513bf7600000000000000000000000000000000000000000000043c2b2187680e25a000",
+				decimals: 18,
+			},
 			&types.InputData{
-				MethodID:  "0xa9059cbb",
-				ToAddress: "5f099b04f1fcea17718e6b91e90990c6849055ee",
-				Value:     "15a3d9f556fd9d800",
+				ToAddress:         "19bbb1c4407dcee04059682949f32cb3b513bf76",
+				Value:             "43c2b2187680e25a000",
+				ValueWithDecimals: "19999.37849",
+				MethodID:          "0xa9059cbb",
 			},
 		},
-		{"Test case 2", "0x", nil},
-		{"Test case 3", "0xa9059cbb000000000000000000000000b714fa495c0332b49d580f2aabd3da667d068b0c00000000000000000000000000000000000000000000000093bbaa5cda828800",
+		{
+			"Test case 2: Transaction Bytom",
+			args{
+				input:    "0xa9059cbb000000000000000000000000f8e2f119e4c9f5bd939cad8ba59dbbf68af109490000000000000000000000000000000000000000000000000000022d191fac50",
+				decimals: 8,
+			},
 			&types.InputData{
-				MethodID:  "0xa9059cbb",
-				ToAddress: "b714fa495c0332b49d580f2aabd3da667d068b0c",
-				Value:     "93bbaa5cda828800",
+				ToAddress:         "f8e2f119e4c9f5bd939cad8ba59dbbf68af10949",
+				Value:             "22d191fac50",
+				ValueWithDecimals: "23927.1829",
+				MethodID:          "0xa9059cbb",
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := ParseInputTx(tt.args)
-			t.Log(got)
-			if !(reflect.DeepEqual(tt.want, got)) {
-				t.Errorf("Failed")
+			if got := ParseInputTx(tt.args.input, tt.args.decimals); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseInputTx() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+func TestConvertInputValueWithDecimal(t *testing.T) {
+	type args struct {
+		val      string
+		decimals int8
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"Test case 1", args{"2b364320ad5f60000", 18}, "49.82", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ConvertInputValueWithDecimal(tt.args.val, tt.args.decimals)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ConvertInputValueWithDecimal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ConvertInputValueWithDecimal() = %v, want %v", got, tt.want)
+			}
+			t.Log(got)
 		})
 	}
 }
